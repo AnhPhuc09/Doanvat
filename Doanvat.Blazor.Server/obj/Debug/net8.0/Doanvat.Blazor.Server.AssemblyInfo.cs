941c96a4e530d9a60d376b0496316e1b22a08690
Gasaiyan <gasaiyan04@gmail.com>
2024-12-05 14:15:29 +0700

    init project

--- Doanvat.Blazor.Server/obj/Debug/net8.0/Doanvat.Blazor.Server.AssemblyInfo.cs
+++ Doanvat.Blazor.Server/obj/Debug/net8.0/Doanvat.Blazor.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Doanvat.Blazor.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a6ec708f67f382249f77ce1a27363b5421edaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Doanvat.Blazor.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Doanvat.Blazor.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.*")]
